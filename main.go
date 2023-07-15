d7d30a7a4c57a6c472e2e966b70b966f81a560c9
Ben Anderson <andersjbe@gmail.com>
2023-07-14 22:45:02 -0400

    Set up CLI prompt and help

--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+
+	fmt.Print("pokedex > ")
+	for ; scanner.Scan(); fmt.Print("pokedex > ") {
+		input := scanner.Text()
+		command, ok := commands[input]
+		if !ok  {
+			fmt.Fprintln(os.Stderr, "command not recognized")
+			continue
+		}
+		command.callback()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+type cliCommand struct {
+	name 					string
+	description 	string
+	callback 			func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Output all available commands",
+			callback: helpCommand,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the program",
+			callback: exitCommand,
+		},
+	}
+}
+
+func helpCommand() error {
+	commands := getCommands()
+
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func exitCommand() error {
+	os.Exit(0)
+	return nil
 }
